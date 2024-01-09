56500382dcbc6aa5988ae3ee0e0380b8fe8f1bc2
Usama <usama-365@protonmail.com>
2024-01-09 21:42:30 +0500

    setup radix UI

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from "@radix-ui/themes";
 import React from "react";
 
 export default function IssuesPage() {
-  return <div>IssuesPage</div>;
+  return (
+    <div>
+      <Button>Create Issue</Button>
+    </div>
+  );
 }
